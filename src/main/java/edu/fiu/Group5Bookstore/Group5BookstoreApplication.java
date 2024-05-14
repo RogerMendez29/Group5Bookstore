8cf04ae4571ae512b8d568bbec003f05ad26973b
RogerMendez29 <Roger61087@gmail.com>
2024-05-14 19:02:48 -0400

    Created Spring boot project

---
+++ src/main/java/edu/fiu/Group5Bookstore/Group5BookstoreApplication.java
@@ -0,0 +1,13 @@
+package edu.fiu.Group5Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fiu/Group5Bookstore/Group5BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fiu.Group5Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group5BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
