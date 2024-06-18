0b19a0ca7665671a99ccd3a952b181a47013863e
melanielozano <melanieperezlozano@outlook.com>
2024-06-18 19:20:29 -0400

    Merge remote-tracking branch 'origin/Melanies_FeatureBranch' into Melanies_FeatureBranch

--- src/main/java/edu/fiu/Group5Bookstore/controller/RatingController.java
+++ src/main/java/edu/fiu/Group5Bookstore/controller/RatingController.java
@@ -2,11 +2,11 @@ package edu.fiu.Group5Bookstore.controller;
 
 import edu.fiu.Group5Bookstore.DTOs.RatingPostDTO;
 import edu.fiu.Group5Bookstore.model.Book;
-import edu.fiu.Group5Bookstore.model.CartItem;
+import edu.fiu.Group5Bookstore.model.Comment;
 import edu.fiu.Group5Bookstore.model.Rating;
 import edu.fiu.Group5Bookstore.model.User;
 import edu.fiu.Group5Bookstore.service.BookService;
-import edu.fiu.Group5Bookstore.service.CartService;
+import edu.fiu.Group5Bookstore.service.CommentService;
 import edu.fiu.Group5Bookstore.service.RatingService;
 import edu.fiu.Group5Bookstore.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -22,35 +22,36 @@ public class RatingController {
 
     private final BookService bookService;
     private final UserService userService;
-    private final CartService cartService;
-    private RatingService ratingService;
+    private final RatingService ratingService;
 
-    public RatingController(RatingService ratingService, UserService userService, BookService bookService, CartService cartService) {
+    public RatingController(RatingService ratingService, UserService userService, BookService bookService) {
         this.ratingService = ratingService;
         this.userService = userService;
-        this.cartService =  cartService;
         this.bookService = bookService;
     }
 
-    @GetMapping("/book/{user_id}")
-    public ResponseEntity<List<Rating>> getRating(@PathVariable Integer user_id) {
-        List<Rating> ratingFound = ratingService.getRatingFromUserId(user_id);
-        return new ResponseEntity<>(ratingFound, HttpStatus.OK);
-    }
-
-    @PostMapping("/createRating")
-    public ResponseEntity<Integer> createRating(@RequestBody RatingPostDTO ratingPostDTO) {
+    //create a rating for a book by a user on a 5-star scale with a datestamp
+    @PostMapping("/rating/{userId}/{bookId}")
+    public ResponseEntity<Rating> createRating(@RequestBody RatingPostDTO ratingPostDTO) {
         User foundUser =  userService.findUser(ratingPostDTO.getUserId());
         Book foundBook = bookService.findBook(ratingPostDTO.getBookId());
-        try {
-            Rating createRating = ratingService.createRating(foundBook, foundUser, ratingPostDTO.getRating());
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (foundUser != null && foundBook != null) {
+            Rating createRating = ratingService.createRating(foundBook, foundUser, ratingPostDTO.getRating(), ratingPostDTO.getDatestamp());
+            return new ResponseEntity<Rating>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<Rating>(HttpStatus.BAD_REQUEST);
         }
     }
 
-
-
+    @GetMapping("/averageRating/{bookId}")
+    public ResponseEntity<List<Rating>> getAverageRatingFromBookId(@PathVariable int bookId) {
+        Book foundBook = bookService.findBook(bookId);
+        if (foundBook != null) {
+            double averageRating = ratingService.getAverageRatingFromBookId(foundBook.getId());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+    }
 }
 
